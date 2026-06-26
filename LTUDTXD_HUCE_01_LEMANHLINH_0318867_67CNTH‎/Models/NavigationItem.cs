8ec558f659fb444849252a4dcecce43d69767978
tranhongquan-1657265 <tranhongquan05112002@gmail.com>
2026-06-26 13:51:43 +0700

    Add files via upload

---
+++ LTUDTXD_HUCE_01_LEMANHLINH_0318867_67CNTH‎/Models/NavigationItem.cs
@@ -0,0 +1,12 @@
+namespace LTUDTXD_HUCE_01_LEMANHLINH_0318867_67TH3.Models;
+
+public sealed class NavigationItem
+{
+    public string Key { get; set; } = string.Empty;
+
+    public string Icon { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
